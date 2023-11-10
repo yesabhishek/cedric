34d6de3cb433c6cd421e38be7c9efaf234ea944b
yesabhishek <choudhuryabhishek76@gmail.com>
2023-11-11 00:32:19 +0530

    Create settings.py

---
+++ core/settings.py
@@ -0,0 +1,13 @@
+"""
+Django settings for my_django_setup project.
+"""
+
+import os
+
+# ... (existing settings)
+
+# Custom Configuration
+USE_DOCKER = True
+CLOUD_PROVIDER = 'AWS'
+CSS_FRAMEWORK = 'TailwindCSS'
+DATABASE_ENGINE = 'sqlite3'
