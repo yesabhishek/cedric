5301213e757a2d00d0830b3eb6a13bd270d1e7fb
yesabhishek <choudhuryabhishek76@gmail.com>
2023-11-11 13:10:40 +0530

    update for test deployment

--- setup.py
+++ setup.py
@@ -1,16 +1,22 @@
 from setuptools import setup, find_packages
 
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
 setup(
-    name='my_django_setup',
-    version='0.1',
-    packages=find_packages(),
-    install_requires=[
-        'Django',
-        # Other dependencies
+    name="yesabhishek",
+    version="0.0.1",
+    author="Abhishek Choudhury",
+    author_email="choudhuryabhishek76@gmail.com",
+    description="Gecko is a Python library designed to streamline the process of setting up a Django application. ",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/pypa/gecko",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
     ],
-    entry_points={
-        'console_scripts': [
-            'my-django-setup=my_django_setup.cli:main',
-        ],
-    },
 )
